a1d734dcc3eaec37bd84ef8d472e5251bed59dd8
Abe011118 <xcalibur023@gmail.com>
2022-11-26 15:08:27 +0800

    This is updated News Website with automated testing

--- main.js
+++ main.js
@@ -18,17 +18,55 @@ export var newsList = [
 
 export function search() {
   //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
+  // const inputValue = document.getElementById("search-input").value;
 
-  //Write your code here for the search function
+  const inputValue = "What";// enable for test only
+
+  function searchFor(arr, query) {
+      function condition(element) {
+          return element.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+      }
+      return arr.filter(condition);
+  }
+
+  const searchArr = (arr, query) => arr.filter(element => element.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  return searchArr(newsList, inputValue);
 }
 
+let flag;
 export function sort(type) {
   if (type == "ascending") {
     //Write your code here for sorting (ascending)
+
+    newsList.sort();
+    flag = true;
   } else {
     //Write your code here for sorting (descending)
-  }
 
+    if(flag == true) {
+      newsList.reverse();
+      flag = false;
+    }
+  }
+  // console.log(newsList);
   return newsList;
 }
+
+
+
+
+
+
+
+
+
+  // search a piece of string or a character from a array elements
+  // let searchArrayWStr = [];
+  // for (let i = 0; i < newsList.length; i++) {
+    
+  //   if(newsList[i].includes(inputValue) == true) {// check array with the search string
+  //     // console.log(`Array position `+ i + " contain the String " + inputValue);
+  //     searchArrayWStr.push(newsList[i]);
+  //   }
+  // }
+  // return searchArrayWStr;
--- main.test.js
+++ main.test.js
@@ -1,6 +1,43 @@
 import { sort, search, newsList } from "./main.js";
 
-//Example testing for search function
-test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+test("Search function testing ", () => {
+  expect(search()).toContain("What films should an aspiring journalist watch?");
 });
+
+// "What films should an aspiring journalist watch?",
+// "Buried underpants and tea bags help scientists evaluate soil",
+// "Decoder: Mining asteroids for minerals can help spare Earth",
+// "Media glare can enrich tennis pros yet imperil mental health",
+// "'Nightmare' TV show 'Euphoria — health threat or high art?",
+// "Decoder: Armenia in a bind as Ukraine war resets global order",
+// "What books should an aspiring journalist read?",
+// "Marie Colvin shined a light on war-torn corners of the world",
+
+test("Sort A-Z function testing ", () => {
+    expect(sort("ascending")).toEqual([
+        // sort up - down
+        "'Nightmare' TV show 'Euphoria — health threat or high art?", 
+        'Buried underpants and tea bags help scientists evaluate soil', 
+        'Decoder: Armenia in a bind as Ukraine war resets global order', 
+        'Decoder: Mining asteroids for minerals can help spare Earth', 
+        'Marie Colvin shined a light on war-torn corners of the world', 
+        'Media glare can enrich tennis pros yet imperil mental health', 
+        'What books should an aspiring journalist read?', 
+        'What films should an aspiring journalist watch?'
+    ]);
+  });
+
+test("Sort Z-A function testing ", () => {
+expect(sort("descending")).toEqual([
+        // sort down - up
+        'What films should an aspiring journalist watch?', 
+        'What books should an aspiring journalist read?', 
+        'Media glare can enrich tennis pros yet imperil mental health', 
+        'Marie Colvin shined a light on war-torn corners of the world', 
+        'Decoder: Mining asteroids for minerals can help spare Earth', 
+        'Decoder: Armenia in a bind as Ukraine war resets global order', 
+        'Buried underpants and tea bags help scientists evaluate soil', 
+        "'Nightmare' TV show 'Euphoria — health threat or high art?"
+    ]);
+});
+
